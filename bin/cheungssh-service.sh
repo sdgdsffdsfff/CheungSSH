16525d2302bb5e5e83f2f3cb5d25bc079885fcab
zhangqichuan <qichuan.zhang@qq.com>
2015-11-30 10:03:30 +0800

    W 2.26.9 11-30
    更新内容：
    1 计划任务问题
    2 命令历史记录
    3 命令拦截过滤
    4 登录审计

--- bin/cheungssh-service.sh
+++ bin/cheungssh-service.sh
@@ -24,11 +24,7 @@ start(){
 			else
 				cnf="/etc/mysql/my.cnf"
 			fi
-<<<<<<< HEAD
 			mysql_sock=`grep  -E '^ *socket.*mysql.*sock$' $cnf|awk  -F  '=' 'NR==1{print  $NF}'`
-=======
-			mysql_sock=`grep  -E '^ *socket.*mysql.sock' $cnf|awk  -F  '=' 'NR==1{print  $NF}'`
->>>>>>> origin/master
 			if [ ! -e $dest_mysql_sock ]
 			then
 				ln -s $mysql_sock  $dest_mysql_sock
